a41a7d4be0c7db45d225041edce3fd715620e62f
lnpmir1 <lnpmir1@USDHCNU4049WPW.us.aegon.com>
2020-01-31 12:23:23 +0100

    updated the annotation

--- spring-security-jwt/src/main/java/mishra/aruni/springsecurityjwt/HelloResource.java
+++ spring-security-jwt/src/main/java/mishra/aruni/springsecurityjwt/HelloResource.java
@@ -1,9 +1,9 @@
 package mishra.aruni.springsecurityjwt;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HelloResource {
 
 	@RequestMapping({"/hello"})
